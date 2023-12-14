15ae94033710f3addca781e932cbc96ba01273af
Orkhan Hajizada <orkhanmh@code.edu.az>
2023-12-14 22:21:05 +0400

    login process

---
+++ LoginProcessWithTuple/Program.cs
@@ -0,0 +1,19 @@
+﻿(string username, string password) user = ("admin", "123");
+
+while (true)
+{
+    Console.Write("İstifadəçi adını daxil edin: ");
+    var inputUsername = Console.ReadLine();
+
+    Console.Write("Şifrənizi daxil edin: ");
+    var inputPassword = Console.ReadLine();
+
+    if (inputUsername == user.username && inputPassword == user.password)
+    {
+        Console.WriteLine("Giriş uğurla tamamlandı!");
+        break;
+    }
+
+    Console.WriteLine("İstifadəçi adı və ya şifrə yanlışdır!. Yenidən cəhd edin!");
+}
+
